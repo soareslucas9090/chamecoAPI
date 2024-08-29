91c8b05f156571934c94730eff6b89d168c2fb77
Lucas Soares <soareslukas9090@hotmail.com>
2024-08-29 17:54:46 -0300

    feat: modelagem do banco e primeira migração + remoção do rest framework JWT

--- chameco/settings.py
+++ chameco/settings.py
@@ -27,7 +27,6 @@ DATABASES = {
         "PORT": os.environ.get("bdPort"),
     }
 }
-signing_key = os.environ.get("secretKeyJWT")
 
 INTERNAL_IPS = [
     "127.0.0.1",
@@ -74,8 +73,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "corsheaders",
-    "rest_framework",
-    "rest_framework_simplejwt",
     "drf_spectacular",
     "drf_spectacular_sidecar",
     "chamecoapi",
@@ -127,35 +124,6 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
-
-
-REST_FRAMEWORK = {
-    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ),
-    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 10,
-    "DATE_INPUT_FORMATS": ["%Y-%m-%d", "%d-%m-%Y", "%Y/%m/%d", "%d/%m/%Y"],
-    "TIME_INPUT_FORMATS": [
-        "%H:%M",
-    ],
-}
-
-SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
-    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=240),
-    "BLACKLIST_AFTER_ROTATION": False,
-    "SIGNING_KEY": signing_key,
-    "AUTH_HEADER_TYPES": ("Bearer",),
-    "TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainPairSerializer",
-    "TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSerializer",
-    "TOKEN_VERIFY_SERIALIZER": "rest_framework_simplejwt.serializers.TokenVerifySerializer",
-    "TOKEN_BLACKLIST_SERIALIZER": "rest_framework_simplejwt.serializers.TokenBlacklistSerializer",
-    "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
-    "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
-}
 
 
 # Internationalization
--- chamecoapi/models.py
+++ chamecoapi/models.py
@@ -1,3 +1,105 @@
 from django.db import models
 
-# Create your models here.
+
+class Usuarios(models.Model):
+    id_cortex = models.IntegerField(null=False)
+    id_setor = models.IntegerField(null=False)
+    id_tipo = models.IntegerField(null=False)
+    chaves_autorizadas = models.ManyToManyField("Chaves", through="PessoasAutorizadas")
+
+    class Meta:
+        verbose_name = "Usuario"
+        verbose_name_plural = "Usuarios"
+
+
+class Blocos(models.Model):
+    nome = models.CharField(max_length=128, null=False)
+
+    class Meta:
+        verbose_name = "Bloco"
+        verbose_name_plural = "Blocos"
+
+    def __str__(self) -> str:
+        str = f"{self.nome}"
+        return str
+
+
+class Salas(models.Model):
+    nome = models.CharField(max_length=128, null=False)
+    bloco = models.ForeignKey(
+        Blocos, related_name="salas", on_delete=models.CASCADE, null=False
+    )
+
+    class Meta:
+        verbose_name = "Sala"
+        verbose_name_plural = "Salas"
+
+    def __str__(self) -> str:
+        str = f"{self.nome}"
+        return str
+
+
+class Chaves(models.Model):
+    sala = models.OneToOneField(
+        Salas, related_name="chave", on_delete=models.CASCADE, null=False
+    )
+    disponivel = models.BooleanField(default=True, null=False)
+    usuarios_autorizados = models.ManyToManyField(
+        Usuarios, through="PessoasAutorizadas"
+    )
+
+    class Meta:
+        verbose_name = "Chave"
+        verbose_name_plural = "Chaves"
+
+    def __str__(self) -> str:
+        str = f"Chave da sala {self.sala.nome}"
+        return str
+
+
+class PessoasAutorizadas(models.Model):
+    usuario = models.ForeignKey(
+        Usuarios,
+        on_delete=models.CASCADE,
+        null=False,
+    )
+    chave = models.ForeignKey(
+        Chaves,
+        on_delete=models.CASCADE,
+        null=False,
+    )
+
+    def __str__(self) -> str:
+        str = f"Usuario: {self.usuario}, chave: {self.chave}"
+        return str
+
+
+class Emprestimos(models.Model):
+    horario_emprestimo = models.DateTimeField(auto_now_add=True)
+    horario_devolucao = models.DateTimeField(null=True)
+    chave = models.ForeignKey(
+        Chaves,
+        related_name="emprestimos_de_chaves",
+        on_delete=models.CASCADE,
+        null=False,
+    )
+    usuario_responsavel = models.ForeignKey(
+        Usuarios,
+        related_name="emprestimos_responsaveis",
+        on_delete=models.CASCADE,
+        null=False,
+    )
+    usuario_solicitante = models.ForeignKey(
+        Usuarios,
+        related_name="emprestimos_solicitados",
+        on_delete=models.CASCADE,
+        null=False,
+    )
+
+    class Meta:
+        verbose_name = "Emprestimo"
+        verbose_name_plural = "Emprestimos"
+
+    def __str__(self) -> str:
+        str = f"Horario: {self.horario_emprestimo}, chave: {self.chave}"
+        return str
