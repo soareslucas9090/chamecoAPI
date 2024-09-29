bc4f6552fee01d734ef498441553d18d0a99523c
nunesemi <emilianunescabral@gmail.com>
2024-09-29 16:54:58 -0300

    feat: criação da views salas

--- chamecoapi/views.py
+++ chamecoapi/views.py
@@ -19,11 +19,12 @@ from rest_framework.viewsets import ModelViewSet
 from .business import getTokens, requestFactory, setTokens
 from .models import Blocos, Chaves, Salas, Usuarios
 from .permissions import IsAdmin, IsTokenValid, IsUserAuthenticated
-from .serializers import (BlocosSerializer, LoginSerializer, SalasSerializer,
-                          UsuariosSerializer)
+from .serializers import (BlocosSerializer, ChavesSerializer, LoginSerializer,
+                          SalasSerializer, UsuariosSerializer)
 
 load_dotenv()
 URL_BASE = os.environ.get("urlBase")
+
 
 class DefaultNumberPagination(PageNumberPagination):
     page_size = 20
@@ -63,7 +64,7 @@ class LoginAPIView(GenericAPIView):
         }
 
         response = requests.post(url, json=body)
-        
+
         if response.status_code == 200:
             access_token = response.json()["access"]
 
@@ -71,7 +72,8 @@ class LoginAPIView(GenericAPIView):
                 "user_id"
             ]
 
-            setTokens(id_user, response.json()["access"], response.json()["refresh"])
+            setTokens(id_user, response.json()[
+                      "access"], response.json()["refresh"])
 
             request.session["id_user"] = id_user
 
@@ -89,22 +91,23 @@ class UsuariosViewSet(ModelViewSet):
     serializer_class = UsuariosSerializer
     pagination_class = DefaultNumberPagination
     permission_classes = [IsTokenValid]
-    
+
     http_method_names = ["get", "post", "put", "delete", "head"]
-    
+
     def get_usuario(self, request: HttpRequest, serializer: dict) -> HttpResponse:
         """
             Método feito para a busca, na API, do ID do usuário do Cortex, pois
             ele sempre precisar ser válido, tanto na criação, quanto edição do usuário
         """
         id_user = request.session.get("id_user")
-        
+
         # Verifica se há um "id_user" guardado na sessão do Django
         if not id_user:
             data = {"status": "error", "detail": "Usuário não logado."}
             return Response(data, status=status.HTTP_401_UNAUTHORIZED)
 
-        url_get_user = f"{URL_BASE}api/gerusuarios/v1/users/{serializer["id_cortex"]}"
+        id_cortex = serializer["id_cortex"]
+        url_get_user = f"{URL_BASE}api/gerusuarios/v1/users/{id_cortex}"
 
         # Faz um get para verificar os detalhes do usuário na API do Cortex
         response = requestFactory("get", url_get_user, id_user)
@@ -114,37 +117,39 @@ class UsuariosViewSet(ModelViewSet):
             try:
                 # Se a resposta for inválida, testa se foi porque o usuário não existe
                 if response.status_code:
-                    data = {"status": "error", "detail": "ID do usuário não existe ou usuário não está autorizado a fazer esta ação."}
+                    data = {
+                        "status": "error", "detail": "ID do usuário não existe ou usuário não está autorizado a fazer esta ação."}
                     return Response(data, status=status.HTTP_400_BAD_REQUEST)
-            
+
             except:
                 # Se a resposta for inválida, e não foi porque o usuário não existe
                 # então é porque o token guardado não é válido
-                data = {"status": "error", "detail": "Token de acesso inválido."}
+                data = {"status": "error",
+                        "detail": "Token de acesso inválido."}
                 return Response(data, status=status.HTTP_401_UNAUTHORIZED)
-            
+
         return response
-         
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer = serializer.validated_data
-        
+
         response = self.get_usuario(request, serializer)
-        
+
         if response.status_code != 200:
             return response
 
         # Criando o usuário caso todas as verificações ocorram bem
         setores = ', '.join(response.json()["nome_setores"])
-        
+
         usuario = Usuarios.objects.create(
             nome=response.json()["nome"],
             id_cortex=response.json()["id"],
             setor=setores,
             tipo=response.json()["nome_tipo"]
         )
-        
+
         result = {
             "status": "success",
             "detail": {
@@ -155,49 +160,48 @@ class UsuariosViewSet(ModelViewSet):
                 "tipo": usuario.tipo,
             }
         }
-        
+
         return Response(result, status=status.HTTP_201_CREATED)
-      
+
     def update(self, request, *args, **kwargs):
         instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=False)
+        serializer = self.get_serializer(
+            instance, data=request.data, partial=False)
         serializer.is_valid(raise_exception=True)
         serializer = serializer.validated_data
-        
 
         if getattr(instance, '_prefetched_objects_cache', None):
             # If 'prefetch_related' has been applied to a queryset, we need to
             # forcibly invalidate the prefetch cache on the instance.
             instance._prefetched_objects_cache = {}
-            
+
         response = self.get_usuario(request, serializer)
-        
+
         if response.status_code != 200:
             return response
-        
+
         # Criando o usuário caso todas as verificações ocorram bem
         setores = ', '.join(response.json()["nome_setores"])
 
         usuario = Usuarios.objects.get(id=kwargs["pk"])
-        
-        usuario.nome=response.json()["nome"]
-        usuario.id_cortex=response.json()["id"]
-        usuario.setor=setores
-        usuario.tipo=response.json()["nome_tipo"]
+
+        usuario.nome = response.json()["nome"]
+        usuario.id_cortex = response.json()["id"]
+        usuario.setor = setores
+        usuario.tipo = response.json()["nome_tipo"]
         if serializer.get("chaves_autorizadas", None):
-            usuario.chaves_autorizadas=serializer["chaves_autorizadas"]
+            usuario.chaves_autorizadas = serializer["chaves_autorizadas"]
 
         usuario.save()
-        
-        return Response(self.get_serializer(usuario).data,status=status.HTTP_200_OK)
-        
-            
+
+        return Response(self.get_serializer(usuario).data, status=status.HTTP_200_OK)
+
     def get_permissions(self):
         if self.request.method in ["PATCH", "DELETE", "PUT", "POST"]:
             return [IsAdmin()]
 
-        return super().get_permissions()    
-        
+        return super().get_permissions()
+
 
 @extend_schema(tags=["Blocos"])
 class BlocosViewSet(ModelViewSet):
@@ -207,17 +211,17 @@ class BlocosViewSet(ModelViewSet):
     permission_classes = [IsTokenValid]
 
     http_method_names = ["get", "post", "put", "delete", "head"]
-    
+
     def get_queryset(self):
         queryset = super().get_queryset()
-        
+
         nome = self.request.query_params.get("nome", None)
-        
+
         if nome:
             queryset = queryset.filter(nome__icontains__iexact=nome)
-            
+
         return queryset
-    
+
     @extend_schema(
         parameters=[
             OpenApiParameter(
@@ -231,7 +235,7 @@ class BlocosViewSet(ModelViewSet):
     )
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
-    
+
     def get_permissions(self):
         if self.request.method in ["PATCH", "DELETE", "PUT", "POST"]:
             return [IsAdmin()]
@@ -240,29 +244,29 @@ class BlocosViewSet(ModelViewSet):
 
 
 @extend_schema(tags=["Salas"])
-class SalasViewSet(ModelViewSet): 
+class SalasViewSet(ModelViewSet):
     queryset = Salas.objects.all()
     serializer_class = SalasSerializer
     pagination_class = DefaultNumberPagination
     permission_classes = [IsTokenValid]
 
     http_method_names = ["get", "post", "put", "delete", "head"]
-    
+
     def get_queryset(self):
         queryset = super().get_queryset()
-        
+
         nome = self.request.query_params.get("nome", None)
-        
+
         if nome:
             queryset = queryset.filter(nome__icontains__iexact=nome)
-            
+
         bloco = self.request.query_params.get("bloco", None)
-        
+
         if bloco:
             queryset = queryset.filter(bloco__nome__icontains__iexact=bloco)
-            
+
         return queryset
-    
+
     @extend_schema(
         parameters=[
             OpenApiParameter(
@@ -283,7 +287,47 @@ class SalasViewSet(ModelViewSet):
     )
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
-    
+
+    def get_permissions(self):
+        if self.request.method in ["PATCH", "DELETE", "PUT", "POST"]:
+            return [IsAdmin()]
+
+        return super().get_permissions()
+
+
+@extend_schema(tags=["Chaves"])
+class ChavesViewSet(ModelViewSet):
+    queryset = Chaves.objects.all()
+    serializer_class = ChavesSerializer
+    pagination_class = DefaultNumberPagination
+    permission_classes = [IsTokenValid]
+
+    http_method_names = ["get", "post", "put", "delete", "head"]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        sala = self.request.get_params.get("sala", None)
+
+        if sala:
+            queryset = queryset.filter(sala_icontains_exact=sala)
+
+        return queryset
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="sala",
+                type=OpenApiTypes.STR,
+                description="Filtrar pelo nome da sala",
+                required=False,
+                location=OpenApiParameter.QUERY,
+            ),
+        ],
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
     def get_permissions(self):
         if self.request.method in ["PATCH", "DELETE", "PUT", "POST"]:
             return [IsAdmin()]
