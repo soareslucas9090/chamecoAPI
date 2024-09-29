931cd9d9f17878d4566f7f9207b61ec09ece25ea
nunesemi <emilianunescabral@gmail.com>
2024-09-29 17:34:45 -0300

    Update urls.py

--- chamecoapi/urls.py
+++ chamecoapi/urls.py
@@ -1,12 +1,19 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
-from .views import BlocosViewSet, LoginAPIView, SalasViewSet, UsuariosViewSet
+from .views import (
+    BlocosViewSet,
+    LoginAPIView,
+    SalasViewSet,
+    UsuariosViewSet,
+    ChavesViewSet,
+)
 
 chameco_router = SimpleRouter()
 chameco_router.register("blocos", BlocosViewSet)
 chameco_router.register("usuarios", UsuariosViewSet)
 chameco_router.register("salas", SalasViewSet)
+chameco_router.register("chaves", ChavesViewSet)
 urlpatterns = [
     ####### API #######
     path("", include(chameco_router.urls)),
